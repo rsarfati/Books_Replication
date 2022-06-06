0aa3a3975660372ff064646deddac500df96288f
Reca Sarfati <rebecca_sarfati@brown.edu>
2022-06-05 23:27:31 -0400

    Last commit before sleeping :)

--- src/helpers.jl
+++ src/helpers.jl
@@ -7,17 +7,15 @@ function ndgrid(v1::AbstractVector{T}, v2::AbstractVector{T}) where {T}
   m, n = length(v1), length(v2)
   v1 = reshape(v1, m, 1)
   v2 = reshape(v2, 1, n)
-  (repeat(v1, 1, n), repeat(v2, m, 1))
+  return repeat(v1, 1, n), repeat(v2, m, 1)
 end
 
 function solveγPar(Dm, D0, dDm, dD0, γ0, p, r)
-
     A = Dm .^ 2.0
     B = (dDm .* r .* p) .+ (r .* Dm) .+ (2.0 .* Dm .* γ0 .* D0)
     C = r .* p .* γ0 .* dD0 .+ r .* γ0 .* D0 .+ γ0 .* γ0 .* D0 .* D0
     γ1 = ((-B .+ ((B .^ 2 .- 4 .* A .* C) .+ 0im) .^ (0.5)) ./ (2 .* A))
     l1 = (real.(γ1) .> 0) .& (imag.(γ1) .≈ 0)
-
     return γ1, l1
 end
 
@@ -34,18 +32,14 @@ end
 
 """
 ```
-function demandshopper(α, β, p, cdid, obsweight)
+function demandshopper(α, β, p, cdid, obs_w)
 ```
 """
-function demandshopper(α, β, p, cdid, obsweight)
+function demandshopper(α, β, p, cdid, obs_w)
     expU = vec(exp.(p .* -α))
-    # sum utility for each book
-    # sum1 = zeros(M)
-    # sum1[1] = sum(expU(1:cdindex[1]))+exp(β)
-    # for j=2:M
-    #     sum1(j)=sum(expU(cdindex(j-1)+1:cdindex(j)))+exp(β)
-    # end
-    sum1 = sum(sparse(collect(1:length(cdid)), vec(cdid), vec(obsweight) .* expU))' + exp(β .* α[1])
+    sum1 = sum(sparse(collect(1:length(cdid)), vec(cdid), vec(obs_w) .* expU))'
+            + exp(β .* α[1])
+
     f1 = expU ./ sum1
     f2 = -α .* expU .* (sum1 .- expU) ./ (sum1 .^ 2)
     f3 = α .^ 2 .* expU .* (sum1 .- expU) .* (sum1 .- 2 .* expU) ./ (sum1 .^ 3)
@@ -64,78 +58,77 @@ Corresponds to Masao/obscalnewtest2015.m
 function obscalnewtest2015(βsigma3, data, basellh, demandcal, p0, rounderr, WFcal)
 
     # [muγ0 α-1 β γishape γimean η-1 r lamda1 lamda2 βcond βpop βlocal olp δ c]
-    numlist      = data["numlist"]
-    localint     = data["localint"]
-    N            = Int(data["N"])
-    conditiondif = data["conditiondif"]
-    cdid         = vec(data["cdid"])
-    obsweight    = vec(data["obsweight"])
-    p            = data["p"]
+    numlist  = data["numlist"]
+    localint = data["localint"]
+    N        = Int(data["N"])
+    cond_dif = data["conditiondif"]
+    cdid     = vec(data["cdid"])
+    obs_w    = vec(data["obsweight"])
+    p        = data["p"]
 
     γ0 = βsigma3[1] .* (numlist .^ βsigma3[8] ./ mean(numlist.^βsigma3[8]))
     α  = fill((βsigma3[2]+1) * βsigma3[14] ^ βsigma3[15], N)
     β  = βsigma3[3] ./ βsigma3[14] ^ βsigma3[15]
     m  = βsigma3[4]
-    γscale = βsigma3[5] ./ m .* (numlist .^ βsigma3[9] ./ mean(numlist .^ βsigma3[9])) .* exp.(βsigma3[12].*localint)
-    η = βsigma3[6]+1 # βsigma3(6*ones(N,1))'+1+βsigma3[11].*popular
-    r = βsigma3[7]
-    βcond = βsigma3[10]
-    olp = βsigma3[13]
-    δ = βsigma3[14]
-    naturaldisappear = βsigma3[16]
+    γscale = βsigma3[5] ./ m .* (numlist .^ βsigma3[9] ./
+                mean(numlist .^ βsigma3[9])) .* exp.(βsigma3[12] .* localint)
+    η      = βsigma3[6]+1
+    r      = βsigma3[7]
+    βcond  = βsigma3[10]
+    olp    = βsigma3[13]
+    δ      = βsigma3[14]
+    nat_disap = βsigma3[16]
 
     ##########
     # Solve for demand and its 1st & 2nd order derivatives
     ##########
-    D0, dD0, d2D0, sumpind, expU = demandshopper(α, β, p0 - βcond .* conditiondif ./ α, cdid, obsweight)
+    D0, dD0, d2D0, sumpind, expU = demandshopper(α, β, p0 - βcond .* cond_dif ./ α, cdid, obs_w)
 
     ## calculate γ1
-    p    = data["p"] .- rounderr
-    Dm   = δ .* (p .^ (-η))
-    dDm  = δ .* (-η) .* (p .^ (-η-1))
-    d2Dm = δ .* (η) .* (η+1) .* (p .^ (-η-2))
-
-    # Solve for the γ that rationalize the price choice
+    p1   = p .- rounderr
+    Dm   = δ .* (p1 .^ (-η))
+    dDm  = δ .* (-η) .* (p1 .^ (-η-1))
+    d2Dm = δ .* (η) .* (η+1) .* (p1 .^ (-η-2))
+    # Solve for the γ rationalizing price choice
     γ1, l1 = solveγPar(Dm, D0 .- rounderr .* dD0 .+ 0.5 .* d2D0 .* rounderr^2, dDm,
-                       dD0 .- rounderr .* d2D0, γ0, p, r)
+                       dD0 .- rounderr .* d2D0, γ0, p1, r)
     ## calculate γ2
-    p    = data["p"] .+ rounderr
-    Dm   = δ .* (p .^ (-η))
-    dDm  = δ .* (-η) .* (p .^ (-η-1))
-    d2Dm = δ .* (η) .* (η+1) .* (p .^ (-η-2))
-
-    #Solve for the γ that rationalize the price choice
+    p2   = p .+ rounderr
+    Dm   = δ .* (p2 .^ (-η))
+    dDm  = δ .* (-η) .* (p2 .^ (-η-1))
+    d2Dm = δ .* (η) .* (η+1) .* (p2 .^ (-η-2))
+    #Solve for the γ rationalizing price choice
     γ2, l2 = solveγPar(Dm, D0 .+ rounderr .* dD0 .+ 0.5 .* d2D0 .* rounderr^2, dDm,
-                       dD0 .+ rounderr .* d2D0, γ0, p, r)
-    SOC = r .* p .* (γ2 .* d2Dm .+ γ0 .* d2D0) .+ 2 .* (r .+ γ2 .* Dm .+ γ0 .*
-            (D0 .+ rounderr .* dD0 .+ 0.5 .* d2D0 .* rounderr^2)) .*
+                       dD0 .+ rounderr .* d2D0, γ0, p2, r)
+
+    SOC = r .* p2 .* (γ2 .* d2Dm .+ γ0 .* d2D0) .+ 2 .* (r .+ γ2 .* Dm .+ γ0 .*
+            (D0 .+ rounderr .* dD0 .+ 0.5 .* d2D0 .* rounderr ^ 2)) .*
             (γ2 .* dDm .+ γ0 .* (dD0 .+ rounderr .* d2D0))
 
     γ1[imag.(γ1) .≈ 0]  .= real(γ1[imag.(γ1) .≈ 0])
     γ2[imag.(γ2) .≈ 0]  .= 0
     γ2[real.(SOC) .> 0] .= 0
-    γ2[real.(γ2) .< 0]  .= .0
+    γ2[real.(γ2) .< 0]  .= 0
 
     ##### WHERE DO NOT MATCH: SOC, γ1, γ2
 
-    profit2 = p ./ (r ./ (γ2 .* Dm + γ0 .* D0) .+ 1)
+    profit2 = p2 ./ (r ./ (γ2 .* Dm + γ0 .* D0) .+ 1)
     profitH = ((r .* (η-1) ./ δ) .^ (-1/η) ./ (1/(η-1)+1)) .* γ2 .^ (1/η)
-    γ3, _ = solveγPar(Dm, 0, dDm, 0, 0, p,r)
+    γ3, _ = solveγPar(Dm, 0, dDm, 0, 0, p2, r)
     γ2[real.(profitH) .> real.(profit2)] .= γ3[real.(profitH) .> real.(profit2)]
 
     γ1[real.(γ1) .< 0] .= .0
     γ1[real.(γ1) .> real.(γ2)] .= γ2[real.(γ1) .> real.(γ2)]
 
-    Dm = δ.*(data["p"].^(-η))
+    Dm = δ.*(p .^ (-η))
 
-    ##
     if demandcal == 1
         disap = data["disappear"]
-        # D0 =demandshopper(α,β,p0- βcond.*data.conditiondif./α,data["cdid"],data["obsweight"]) #
-        demandlh   = (disap > 0) - (2 .* disap - 1) .* exp(-0.166666667 .* (γ0.*D0 + (γ2 + γ1)./2 .* Dm)) .* naturaldisappear
-        demandlhol = (disap > 0) - (2 .* disap - 1) .* # Next line starts probability of nondisappear due to shopper demand.
-                     exp.(-0.166666667.*(γ0.*D0)) .*   # Next line is due to nonshopper demand (taken expectation wrt to γi)
-                     (1 .+ γscale .* 0.166666667 .* Dm) .^ -m .* naturaldisappear
+        # D0 =demandshopper(α,β,p0- βcond.*data.cond_dif./α,data["cdid"],data["obsweight"]) #
+        demandlh   = (disap > 0) - (2 .* disap - 1) .* exp(-0.166666667 .* (γ0.*D0 + (γ2 + γ1)./2 .* Dm)) .* nat_disap
+        demandlhol = (disap > 0) - (2 .* disap - 1) .*  # Next line starts probability of nondisappear due to shopper demand
+                        exp.(-0.166666667.*(γ0.*D0)) .* # Next line is due to nonshopper demand (taken expectation wrt to γi)
+                        (1 .+ γscale .* 0.166666667 .* Dm) .^ -m .* nat_disap
 
         lip_o  = min((gamcdf(γ2, m, γscale) - gamcdf(γ1, m, γscale)), 1) .* demandlh .^ 3
         lip_ol = basellh .* demandlhol .^ 3 # Price likelihood. Next line starts disappear likelihood
@@ -148,16 +141,13 @@ function obscalnewtest2015(βsigma3, data, basellh, demandcal, p0, rounderr, WFc
     olppost = olp .* lip_ol ./ liptemp
     lip = log.(liptemp)
 
-    pi_v = zeros(N)
-    CSns = zeros(N)
-    CSs  = zeros(N)
+    pi_v, CSns, CSs = zeros(N), zeros(N), zeros(N)
 
     if WFcal
-        pi_v, CSns, CSs = welfaresimple(γ1, γ2, γscale.*m, γ0, olppost, Dm, D0, p0, data, [α[1] β η r])
+        pi_v, CSns, CSs = welfaresimple(γ1, γ2, γscale .* m, γ0, olppost, Dm, D0, p0, data, vcat(α[1], β, η, r))
     end
     return lip, γ2, γ1, γ0, D0, Dm, pi_v, CSns, CSs
 end
-
 
 """
 ```

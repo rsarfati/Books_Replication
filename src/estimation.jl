f32d761d47b19a7e1c7e8e3d6db6ef0aed9277dc
Reca Sarfati <rebecca_sarfati@brown.edu>
2023-01-02 15:22:25 -0500

    New optim

--- src/estimation.jl
+++ src/estimation.jl
@@ -119,8 +119,8 @@ function estimate_model(; # Data specification
 
 	# Optimize objective function, then reconstitute optimal parameter
 	# vector to again include fixed/calibrated parameters.
-	res = optimize(obj_fun, #lb[free_ind], ub[free_ind],
-				   θ_val[free_ind], Newton(),#Fminbox(),
+	res = optimize(obj_fun, lb[free_ind], ub[free_ind],
+				   θ_val[free_ind], ConjugateGradient(), 
 				   Optim.Options(f_calls_limit = Int(1e5), iterations = Int(1e5),
 		     	   show_trace = true, store_trace = true))
 	θ, llh = θ_full(res.minimizer), res.minimum

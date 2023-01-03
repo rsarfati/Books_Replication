34eb76be010ad1b21c21108568e1b1dcf8fca624
Reca Sarfati <rebecca_sarfati@brown.edu>
2023-01-02 20:25:07 -0500

    LBFGS()

--- src/estimation.jl
+++ src/estimation.jl
@@ -120,7 +120,7 @@ function estimate_model(; # Data specification
 	# Optimize objective function, then reconstitute optimal parameter
 	# vector to again include fixed/calibrated parameters.
 	res = optimize(obj_fun, lb[free_ind], ub[free_ind],
-				   θ_val[free_ind], Fminbox(),#NelderMead(),
+				   θ_val[free_ind], LBFGS(),#Fminbox(),#NelderMead(),
 				   Optim.Options(f_calls_limit = Int(1e5), iterations = Int(1e5),
 		     	   show_trace = true, store_trace = true))
 	θ, llh = θ_full(res.minimizer), res.minimum

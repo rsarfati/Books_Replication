647ab67896978af00baf42063996d6559f990aac
Reca Sarfati <rebecca_sarfati@brown.edu>
2023-07-30 14:03:52 -0400

    launch script fixed

--- src/launch_script.jl
+++ src/launch_script.jl
@@ -1,5 +1,5 @@
 using CSV, DataFrames, Dates, DelimitedFiles, Distributed, Distributions, FileIO
-using FixedEffectModels, JLD2, MAT, Optim, OrderedCollections
+using JLD2, MAT, Optim, OrderedCollections
 using Printf, Random, Roots, SparseArrays, Statistics, UnPack
 println("Packages loaded!")
 

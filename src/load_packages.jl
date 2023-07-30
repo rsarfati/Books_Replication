538c9ce6d29c1436fe1e4848f7bb93024efaf04f
Reca Sarfati <rebecca_sarfati@brown.edu>
2023-07-30 17:12:10 -0400

    Create a log directory

--- src/load_packages.jl
+++ src/load_packages.jl
@@ -10,10 +10,12 @@ using Printf, Random, Roots, SparseArrays, Statistics, UnPack
 println("Packages loaded!")
 
 ## Build output folders if don't exist
+global LOGS   = "$path/../output/log"
 global OUTPUT = "$path/../output/data"
 global INPUT  = "$path/../input"
 
 !isdir("$path/../output/")	&& run(`mkdir $path/../output/`)
+!isdir("$LOG/")				&& run(`mkdir $LOG/`)
 !isdir("$OUTPUT/")			&& run(`mkdir $OUTPUT/`)
 !isdir("$OUTPUT/../plots")	&& run(`mkdir $OUTPUT/../plots/`)
 !isdir("$OUTPUT/../tables")	&& run(`mkdir $OUTPUT/../tables/`)
